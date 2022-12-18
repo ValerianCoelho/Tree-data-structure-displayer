b6a95a6f4c9a27a44627642909d1084e67685420
Valerian Coelho <valerian27012004@gmail.com>
2022-12-18 15:44:17 +0530

    display tree function complete

--- Tree_Displayer.py
+++ Tree_Displayer.py
@@ -7,45 +7,71 @@ def sumOfPowerSeries(noOfTerms):
     return pow(2, noOfTerms) - 1
 
 levelOrder = input().split(' ') # Taking input from the user in level order sequence
-levelOrder = [int(node) for node in levelOrder] # Typecasting all the input to int
 levelOrder = [levelOrder[sumOfPowerSeries(i):sumOfPowerSeries(i+1)] for i in range(logOfPowerSeries(len(levelOrder)))] # Splitting the List (levelOrder) into sub-list's, replicating Level order traversal structure
 
-lines = []
 width = []
+edgeWidth = []
 
 for i in range(len(levelOrder)-1, -1, -1):
+    top = ''; center = ''; bottom = ''; edge = ''
     line = []
-    top = ''
-    center = ''
-    bottom = ''
-    line = []
-    if i == len(levelOrder)-1: # This is a leaf node
 
-        for j in range(len(levelOrder[i])): # Creating the top and bottom portion of the box
+    for j in range(len(levelOrder[i])):
+        subTop = ''; subCenter = ''; subBottom = ''
+        # Top ----------------------------
+        if i == 0: # Belongs to a root node
+            subTop = '┌' + '─'*(len(levelOrder[i][j])+2) + '┐'
+        else: # Belongs to a non-root node
+            subTop = '┌' + f'{"┴":─^{len(levelOrder[i][j])+2}}' + '┐'
 
-            top += '┌' + f'{"┴":─^{len(str(levelOrder[i][j]))+2}}' + '┐'
-            center += '│ ' + str(levelOrder[i][j]) + ' │'
-            bottom += '└' + '─'*(len(str(levelOrder[i][j]))+2) + '┘'
+        # Center -------------------------
+        subCenter = '│ ' + levelOrder[i][j] + ' │'
+            
+        # Bottom -------------------------
+        if i == len(levelOrder)-1: # Belongs to a leaf node
+            subBottom = '└' + '─'*(len(levelOrder[i][j])+2) + '┘'
 
-            if j!=len(levelOrder[i])-1:
-                bottom += ' '
-                top += ' '
-                center += ' '
-    if i == 0: # This is a root node
-        print('Hello')
-    else: # This is for non-(root, leaf) nodes
-        print('Hello')
+            # Appending the width of every node, The width is calculated here because this 'if block' runs only once at the start of the for loop, and that is what we want
+            width.append(len('│ ' + levelOrder[i][j] + ' │'))
+
+        else: # Belongs to a non-leaf node
+            subBottom = '└' + f'{"┬":─^{len(levelOrder[i][j])+2}}' + '┘'
+
+        # Adding the width between nodes, this is done only when the level in not a leaf level
+        if i != len(levelOrder)-1:
+            # Bug encountered here, here j = 1, so width[2*j+1] will be out of range
+            subTop = f'{subTop: ^{width[j]}}'
+            subCenter = f'{subCenter: ^{width[j]}}'
+            subBottom = f'{subBottom: ^{width[j]}}'
+
+            
+
+        # Adding the sub-(top, center and bottom) to (top, center, bottom)
+        top += subTop; center += subCenter; bottom += subBottom
+
+        # Adding one whitespace padding between nodes
+        if j != len(levelOrder[i])-1:
+            top += ' '; center += ' '; bottom += ' '
+    if i != 0:
+        edgeWidth = [sum(width[0:k]) + k + int(width[k]/2) for k in range(len(width))]
+        edgeline = ''
+        for k in range(int(len(edgeWidth)/2)):
+            print(width[2*k+1]-edgeWidth[2*k])
+            subEdgeLine = ' '*edgeWidth[2*k] + '┌' + f'{"┴": ^{width[2*k+1]-edgeWidth[2*k]}}' + '┐' + '   '
+            edgeline += subEdgeLine
+        print(edgeline)
 
 
-    line.append(top)
-    line.append(center)
-    line.append(bottom)
-    lines.append(line)
+    print(edgeWidth)
+    width = [width[2*x]+width[2*x+1]+1 for x in range(int(len(width)/2))]
 
-print(lines)
-
-# print(levelOrder)
-# print(nodeLinesSum)
-
+    print(top)
+    print(center)
+    print(bottom)
 
 # lines-> line-> (top, center, bottom)
+
+# 1 2 3 4 5 6 7
+# ['4', '5', '6', '7']
+# ['2', '3']
+# ['1']
--- Tree_Displayer_2.py
+++ Tree_Displayer_2.py
@@ -9,48 +9,82 @@ def sumOfPowerSeries(noOfTerms):
 levelOrder = input().split(' ') # Taking input from the user in level order sequence
 levelOrder = [levelOrder[sumOfPowerSeries(i):sumOfPowerSeries(i+1)] for i in range(logOfPowerSeries(len(levelOrder)))] # Splitting the List (levelOrder) into sub-list's, replicating Level order traversal structure
 
+lines = []
 width = []
+edges = []
 
 for i in range(len(levelOrder)-1, -1, -1):
     top = ''; center = ''; bottom = ''
     line = []
+    edge = ''
 
     for j in range(len(levelOrder[i])):
+        subTop = ''; subCenter = ''; subBottom = ''
         # Top ----------------------------
         if i == 0: # Belongs to a root node
-            top = top + '┌' + '─'*(len(levelOrder[i][j])+2) + '┐'
+            subTop = '┌' + '─'*(len(levelOrder[i][j])+2) + '┐'
         else: # Belongs to a non-root node
-            top = top + '┌' + f'{"┴":─^{len(levelOrder[i][j])+2}}' + '┐'
+            subTop = '┌' + f'{"┴":─^{len(levelOrder[i][j])+2}}' + '┐'
 
         # Center -------------------------
-        center = center + '│ ' + levelOrder[i][j] + ' │'
+        subCenter = '│ ' + levelOrder[i][j] + ' │'
             
         # Bottom -------------------------
         if i == len(levelOrder)-1: # Belongs to a leaf node
-            bottom = bottom + '└' + '─'*(len(levelOrder[i][j])+2) + '┘'
+            subBottom = '└' + '─'*(len(levelOrder[i][j])+2) + '┘'
 
             # Appending the width of every node, The width is calculated here because this 'if block' runs only once at the start of the for loop, and that is what we want
             width.append(len('│ ' + levelOrder[i][j] + ' │'))
 
         else: # Belongs to a non-leaf node
-            bottom = bottom + '└' + f'{"┬":─^{len(levelOrder[i][j])+2}}' + '┘'
+            subBottom = '└' + f'{"┬":─^{len(levelOrder[i][j])+2}}' + '┘'
 
         # Adding the width between nodes, this is done only when the level in not a leaf level
         if i != len(levelOrder)-1:
-            top = f'{top: ^{width[2*j]+width[2*j+1]+1}}'
-            center = f'{center: ^{width[2*j]+width[2*j+1]+1}}'
-            bottom = f'{bottom: ^{width[2*j]+width[2*j+1]+1}}'
+            # Bug encountered here, here j = 1, so width[2*j+1] will be out of range
+            subTop = f'{subTop: ^{width[j]}}'
+            subCenter = f'{subCenter: ^{width[j]}}'
+            subBottom = f'{subBottom: ^{width[j]}}'
+
+            
+
+        # Adding the sub-(top, center and bottom) to (top, center, bottom)
+        top += subTop; center += subCenter; bottom += subBottom
 
         # Adding one whitespace padding between nodes
         if j != len(levelOrder[i])-1:
             top += ' '; center += ' '; bottom += ' '
+    if i != 0:
+        edgeWidth = [sum(width[0:k]) + k + int(width[k]/2) for k in range(len(width))]
+        for k in range(int(len(levelOrder[i])/2)):
+            subEdge = ' '*int((width[2*k])/2) + '┌' + f'{"┴":─^{edgeWidth[2*k+1]-edgeWidth[2*k]-1}}' + '┐' + ' '*(int(width[2*k+1]/2)) + ' '
+            edge += subEdge
+        edges.append(edge)
+        
 
-    print(top)
-    print(center)
-    print(bottom)
-    print(width)
+    width = [width[2*x]+width[2*x+1]+1 for x in range(int(len(width)/2))]
 
+    line.append(top)
+    line.append(center)
+    line.append(bottom)
+
+    lines.append(line)
+
+lines.reverse()
+edges.reverse()
+
+
+
+for i in range(len(lines)):
+    if i != 0:
+        print(edges[i-1])
+    for j in range(len(lines[i])):
+        print(lines[i][j])
+    
 # lines-> line-> (top, center, bottom)
+
+# print(edges)
+# print(lines)
 
 # 1 2 3 4 5 6 7
 # ['4', '5', '6', '7']
